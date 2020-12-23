ca28a7dc639d0a5fd3b2b7daf8d594d02cbb133e
darkzwer <darkzwer@yandex.ru>
2020-12-24 00:17:47 +0200

    First Bild

--- What Number/main.swift
+++ What Number/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
-
+let randomNumber = UInt8(arc4random_uniform(50))
+print ("Компьютер загадает число вам необходимо угадать его.")
+var myNumber: String?
+repeat {
+    print ("Введите ваш вариант и нажмите Enter")
+    myNumber = readLine()
+    if (UInt8(myNumber!) == randomNumber){
+        print("Вы угадали")
+    } else if (UInt8(myNumber!)! > randomNumber){
+        print("Ваше число больше")
+    } else if (UInt8(myNumber!)! < randomNumber){
+        print("Ваше число меньше")
+    }
+} while randomNumber != UInt8(myNumber!)
